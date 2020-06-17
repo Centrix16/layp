5b248aac16740fcce49db942b3d0b6a08b921542
Centrix16 <tel1tsyn@yandex.ru>
2020-06-17 22:25:46 +0300

    add tree.h

---
+++ tree.h
@@ -0,0 +1,18 @@
+#ifndef __TREE_H_INCLUDED__
+#define __TREE_H_INCLUDED__
+
+#define TREE_DATA_LEN
+
+typedef struct {
+	char *data;
+	int n;
+	struct tree **list;
+} tree;
+
+void tree_init(tree *tr);
+void tree_set_data(tree *tr, char *data);
+void tree_set_n(tree *tr, int n);
+void tree_add(tree *tr);
+void tree_free(tree *tr);
+
+#endif
