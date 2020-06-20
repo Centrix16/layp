c0baf95170b723621f7dd7812eb9c4206b8b8523
Centrix16 <tel1tsyn@yandex.ru>
2020-06-20 14:40:05 +0300

    fixed memory leak

--- tree.h
+++ tree.h
@@ -14,5 +14,7 @@ void tree_set_data(tree *tr, char *data);
 void tree_set_n(tree *tr, int n);
 void tree_add(tree *tr);
 void tree_free(tree *tr);
+void tree_free_data(tree *tr);
+void tree_free_n(tree *tr);
 
 #endif
